b4f48449735b27c303e754572260d8037cb2dfa5
Aritra Mondal <aritra041999.dev@gmail.com>
2024-08-14 23:51:46 +0200

    chroe: split test utils into setup and teardown

--- tests/setup.go
+++ tests/setup.go
@@ -73,16 +73,3 @@ func Setup(t *testing.T, numberOfDBs int) []PostgresContainerMeta {
 
 	return containerMetaList
 }
-
-func TeardownPostgresContainer(containerMeta PostgresContainerMeta) {
-	if err := containerMeta.Container.Terminate(containerMeta.Context); err != nil {
-		log.Fatalf("failed to terminate container: %s", err)
-	}
-}
-
-func Teardown(containerMetas []PostgresContainerMeta) {
-	log.Println("tearing down test containers")
-	for _, containerMeta := range containerMetas {
-		TeardownPostgresContainer(containerMeta)
-	}
-}
