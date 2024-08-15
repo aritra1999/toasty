7b2649d153edc8b38035bbf50d845a29bca89fe5
Aritra Mondal <aritra041999.dev@gmail.com>
2024-08-15 15:52:19 +0200

    chore: close db connection

--- core/query.go
+++ core/query.go
@@ -21,14 +21,15 @@ func ProcessQuery(configs []ConnectionConfig, query string) {}
 
 func ExecuteQuery(config ConnectionConfig, query string) ([]interface{}, error) {
 	connStr := fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=disable", config.Username, config.Database, config.Password, config.Host, strconv.Itoa(config.Port))
+
 	db, err := sql.Open("postgres", connStr)
+	defer db.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	rows, err := db.Query(query)
 	defer rows.Close()
-
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
